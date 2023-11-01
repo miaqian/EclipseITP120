ed5995fd1daa2ad693a53349d23c5db6d2e9f91e
miaQian <53013893+miaqian@users.noreply.github.com>
2023-11-01 00:53:05 -0400

    create Array package

---
+++ ITP120/src/Array/ArraySelfPractice.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package Array;
+
+/**
+ * NV
+ * Follow it with additional details about its purpose
+ * and how to use it.
+ *
+ * @author huiziqian
+ * @version Oct 31, 2023
+ *
+ */
+public class ArraySelfPractice {
+
+    /**
+     * main method
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
