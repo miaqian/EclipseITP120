6e5d1778dcc535cb71c86ccead3d328eca7fb86f
miaQian <53013893+miaqian@users.noreply.github.com>
2023-11-01 00:53:59 -0400

    Update ArraySelfPractice.java

--- ITP120/src/Array/ArraySelfPractice.java
+++ ITP120/src/Array/ArraySelfPractice.java
@@ -4,12 +4,20 @@
 package Array;
 
 /**
- * NV
- * Follow it with additional details about its purpose
- * and how to use it.
+ * notes about Arrays and the ArrayList Class
+ *1.Processing Array Content. Passing Arrays as Arguments to Methods
+ *2.Some useful Array Algorithms and operations
+ *3.Returning array from methods
+ *4.String Arrays
+ *5.Array of Objects
+ *6.The Sequential Search Algorithm
+ *7.Parallel Array
+ *8.Arrays with Three or more Dimensions
+ *8.Command-Line Arguments
+ *9.ArrayList Class
  *
  * @author huiziqian
- * @version Oct 31, 2023
+ * @version October 31, 2023
  *
  */
 public class ArraySelfPractice {
@@ -17,9 +25,15 @@ public class ArraySelfPractice {
     /**
      * main method
      *
-     * @param args
+     * @param args ignore
      */
     public static void main(String[] args) {
+        //An array is an object, so it needs an object reference
+        //declare a reference to an array that will hold integers.
+        int [] numbers;
+        //The next step creates the array and assigns its address to the numbers variable
+        //create a new array that will hold 6 integers.
+        numbers = new int[6];
 
     }
 
