2d73c32ff7d73424238bcfd16f737fe41eacdaab
miaQian <53013893+miaqian@users.noreply.github.com>
2023-11-10 23:30:25 -0500

    set up lab9

---
+++ ITP120/src/Project2/GraphSquare.java
@@ -0,0 +1,83 @@
+/**
+ *
+ */
+package Project2;
+
+/**
+ * This program accepts 4 sets of coordinates from the user. Each set of coordinates have x and y
+ * values.this class will determine if the 4 points can form either a square or rectangle
+ *
+ * @author huiziqian
+ * @version Nov 10, 2023
+ *
+ */
+public class GraphSquare {
+
+    /**
+     * Place a description of your method here.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        calculateSlope();
+        compareSlope();
+        calculateDistance();
+        compareDistance();
+        draw();
+    }
+    /**
+     *
+     * Place a description of your method here.
+     *
+     * @return
+     */
+        private static double draw() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     *
+     * Place a description of your method here.
+     *
+     * @return
+     */
+    private static boolean compareDistance() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     *
+     * Place a description of your method here.
+     *
+     * @return
+     */
+    private static double calculateDistance() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     *
+     * Place a description of your method here.
+     *
+     * @return
+     */
+    private static boolean compareSlope() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     *
+     * Place a description of your method here.
+     *
+     */
+    private static void calculateSlope() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ ITP120/src/string_character_stringbuffer/CircularString.java
@@ -0,0 +1,51 @@
+/**
+ *
+ */
+package string_character_stringbuffer;
+
+/**
+ * This program is to check if the input sentence is a circular sentence .the last character of a
+ * word is equal to the first character of the next word .the last character of the last word is
+ * equal to the first character of the first word
+ *
+ * @author huiziqian
+ * @version Nov 10, 2023
+ *
+ */
+public class CircularString {
+
+    /**
+     * Main method
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * Accepts a string input from the user and converts it to an array of words
+     *
+     * @param sentence
+     * @return sentence is an array
+     */
+    public static String inputString(String sentence) {
+        return sentence;
+
+    }
+
+    /**
+     *
+     * uses the String array as a parameter and returns a boolean value, this method will determine
+     * if the array is a circular
+     *
+     * @param array returned array from inputStrin method
+     * @return true
+     *
+     */
+    public static boolean isCircular(String[] array) {
+        return true;
+    }
+
+}
